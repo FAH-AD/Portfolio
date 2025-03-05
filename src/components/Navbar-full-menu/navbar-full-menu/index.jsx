49a4eb9d042926519f5bc04dfb9aab9ef1fac99b
Fahad Ali <130178840+FAH-AD@users.noreply.github.com>
2025-03-05 05:42:56 +0500

    initial commit

---
+++ src/components/Navbar-full-menu/navbar-full-menu/index.jsx
@@ -0,0 +1,122 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import Split from "../../Split";
+import Link from "next/link";
+import appData from "../../../data/app.json";
+import handleFullScreenNavbar from "../../../common/handleFullScreenNavbar";
+
+const NavbarFullMenu = ({ theme, lr }) => {
+  React.useEffect(() => {
+    handleFullScreenNavbar();
+  }, []);
+  return (
+    <>
+      <div
+        id="navi"
+        className={`topnav ${theme ? (theme === "light" ? "light" : "") : ""}`}
+      >
+        <div className="container-fluid">
+          <div className="logo">
+            <a href="#0">
+              {theme ? (
+                theme === "light" ? (
+                  <h4>Softigenix</h4>
+                ) : (
+                  <h4>Softigenix</h4>
+                )
+              ) : (
+                <h4>Softigenix</h4>
+              )}
+            </a>
+          </div>
+          <div className="menu-icon">
+            <span className="icon">
+              <i></i>
+              <i></i>
+            </span>
+            <Split>
+              <span className="text" data-splitting>
+                <span className="menu-text">Menu</span>
+              </span>
+            </Split>
+          </div>
+        </div>
+      </div>
+
+      <div className="hamenu">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-9 col-md-8">
+              <div className="menu-links">
+                <ul className="main-menu">
+                  <li>
+                  <div className="o-hidden">
+                      <Link href="/">
+                        <a className="link">
+                          <span className="nm">01.</span>Home
+                        </a>
+                      </Link>
+                    </div>
+                   
+                  </li>
+                  <li>
+                    <div className="o-hidden">
+                      <Link href="/about/about-dark">
+                        <a className="link">
+                          <span className="nm">02.</span>About Us
+                        </a>
+                      </Link>
+                    </div>
+                  </li>
+                  <li>
+                  <div className="o-hidden">
+                      <Link href="/about/about-dark">
+                        <a className="link" href="/showcase/showcase-dark" >
+                          <span className="nm">03.</span>Portfolio
+                        </a>
+                      </Link>
+                    </div>
+                
+                  </li>
+
+
+                  <li>
+                    <div className="o-hidden">
+                      <Link href="/contact/contact-dark">
+                        <a className="link">
+                          <span className="nm">04.</span>Contact
+                        </a>
+                      </Link>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4">
+              <div className="cont-info">
+                <div className="item">
+                  <h6>Phone :</h6>
+                  <p>+92 313-0471004</p>
+                </div>
+                <div className="item">
+                  <h6>Address :</h6>
+                  <p>
+                  A32 ,Kalma Heights,Lahore Pakistan.
+                  </p>
+                </div>
+                <div className="item">
+                  <h6>Email :</h6>
+                  <p>
+                    <a href="#0">softigenix@gmail.com</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavbarFullMenu;
